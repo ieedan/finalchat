13b6f91a9be6f27358d1f38f56d348107e87b58e
Aidan Bleser <aidanbleser35@gmail.com>
2025-12-08 09:43:44 -0600

    accept only images

--- src/lib/features/chat/components/prompt-input/prompt-input-attachment-button.svelte
+++ src/lib/features/chat/components/prompt-input/prompt-input-attachment-button.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import * as FileDropZone from '$lib/components/ui/file-drop-zone';
-	import PaperclipIcon from '@lucide/svelte/icons/paperclip';
 	import { usePromptInputAttachmentButton } from './prompt-input.svelte.js';
 	import { buttonVariants } from '$lib/components/ui/button/button.svelte';
+	import ImagePlusIcon from '@lucide/svelte/icons/image-plus';
 
 	const attachmentButtonState = usePromptInputAttachmentButton();
 </script>
 
-<FileDropZone.Root onUpload={attachmentButtonState.rootState.onUpload}>
+<FileDropZone.Root
+	onUpload={attachmentButtonState.rootState.onUpload}
+	accept={FileDropZone.ACCEPT_IMAGE}
+>
 	<FileDropZone.Trigger class={buttonVariants({ variant: 'outline', size: 'icon' })}>
-		<PaperclipIcon class="size-4" />
+		<ImagePlusIcon class="size-4" />
 	</FileDropZone.Trigger>
 </FileDropZone.Root>
