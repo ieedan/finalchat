01206fa15b6fadbf8c18208bde6cbc2a225d08d0
Aidan Bleser <aidanbleser35@gmail.com>
2026-01-06 11:02:32 -0600

    update models

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -2,20 +2,20 @@ import z from 'zod';
 import type { Model, ModelId } from './features/chat/types';
 import { tool } from 'ai';
 
-export const TITLE_GENERATION_MODEL = 'google/gemini-2.5-flash' as ModelId;
+export const TITLE_GENERATION_MODEL = 'google/gemini-2.5-flash-lite' as ModelId;
 
 export const DEFAULT_ENABLED_MODEL_IDS = [
 	'google/gemini-3-pro-preview',
 	'anthropic/claude-opus-4.5',
-	'openai/gpt-5.1',
+	'openai/gpt-5.2',
 	'openai/gpt-oss-120b',
-	'google/gemini-2.5-flash',
+	'google/gemini-3-flash-preview',
 	'google/gemini-2.5-flash-lite'
 ] as ModelId[];
 
 export const BASIC_MODELS: Model[] = [
 	{
-		id: 'google/gemini-2.5-flash' as ModelId,
+		id: 'google/gemini-3-flash-preview' as ModelId,
 		name: 'Balanced',
 		description: 'Fast and reliable.'
 	},
@@ -25,7 +25,7 @@ export const BASIC_MODELS: Model[] = [
 		description: 'Fast and cheap model.'
 	},
 	{
-		id: 'openai/gpt-5.1' as ModelId,
+		id: 'openai/gpt-5.2' as ModelId,
 		name: 'Thoughtful',
 		description: 'Smart and detailed.'
 	},
