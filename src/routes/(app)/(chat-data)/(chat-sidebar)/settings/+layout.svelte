c425d67419cca5f9eeafc95d971b7d9aff0d3663
Aidan Bleser <aidanbleser35@gmail.com>
2026-01-14 07:25:29 -0600

    Update +layout.svelte

--- src/routes/(app)/(chat-data)/(chat-sidebar)/settings/+layout.svelte
+++ src/routes/(app)/(chat-data)/(chat-sidebar)/settings/+layout.svelte
@@ -20,13 +20,12 @@
 	{#if sidebar.isMobile || !sidebar.open}{/if}
 	<div
 		class={cn('sticky z-20 top-0 h-0 w-full transition-[height]', {
-			'h-14 settings:h-0 [--height:3.5rem] settings:[--height:0] [--content:block] settings:[--content:none]':
-				sidebar.isMobile || !sidebar.open
+			'h-14 settings:h-0 [--height:3.5rem] settings:[--height:0]': sidebar.isMobile || !sidebar.open
 		})}
 	>
 		<div class="relative h-(--height) top-0">
 			<div class="absolute z-20 h-(--height) bg-background mask-b-from-25% top-0 w-full"></div>
-			<div class="" style="display: var(--content);">
+			<div class={cn('hidden', { 'flex settings:hidden': sidebar.isMobile || !sidebar.open })}>
 				{#if scrollState.y > 15}
 					<animated.span
 						initial={{ opacity: 0, y: -10 }}
