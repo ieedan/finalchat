26eb184f32b0c63f8fc6e85e07c651bca67d5eae
Aidan Bleser <aidanbleser35@gmail.com>
2026-01-27 07:40:13 -0600

    use import optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { sveltekitOG } from '@ethercorps/sveltekit-og/plugin';
+import transformRemixIconImports from 'vite-plugin-transform-remixicon-imports';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), sveltekitOG()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), sveltekitOG(), transformRemixIconImports()],
 	envPrefix: ['PUBLIC_'],
 	test: {
 		expect: { requireAssertions: true },
