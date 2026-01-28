1f29426d64ea6e7ae32d83d42bd1fcce35b48c57
Aidan Bleser <aidanbleser35@gmail.com>
2026-01-28 08:58:57 -0600

    use scroll area with image masks in sidebar

--- src/lib/components/ui/sidebar/sidebar-content.svelte
+++ src/lib/components/ui/sidebar/sidebar-content.svelte
@@ -1,24 +1,40 @@
 <script lang="ts">
 	import type { HTMLAttributes } from 'svelte/elements';
 	import { cn, type WithElementRef } from '$lib/utils.js';
+	import { ScrollArea } from '$lib/components/ui/scroll-area';
+	import { ScrollState } from 'runed';
 
 	let {
 		ref = $bindable(null),
 		class: className,
-		children,
-		...restProps
+		children
 	}: WithElementRef<HTMLAttributes<HTMLElement>> = $props();
+
+	const scrollState = new ScrollState({
+		element: () => ref
+	});
 </script>
 
-<div
-	bind:this={ref}
+<ScrollArea
+	bind:viewportRef={ref}
 	data-slot="sidebar-content"
 	data-sidebar="content"
 	class={cn(
-		'flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden',
+		'flex min-h-0 flex-1 flex-col gap-2 overflow-auto overflow-x-hidden group-data-[collapsible=icon]:overflow-hidden',
 		className
 	)}
-	{...restProps}
 >
+	<div
+		class={cn(
+			'absolute z-20 h-10 bg-sidebar mask-b-from-25% top-0 w-full opacity-0 transition-opacity ease-in-out',
+			!scrollState.arrived.top && 'opacity-100'
+		)}
+	></div>
 	{@render children?.()}
-</div>
+	<div
+		class={cn(
+			'absolute z-20 h-10 bg-background mask-t-from-25% -bottom-px w-full opacity-0 transition-opacity ease-in-out',
+			!scrollState.arrived.bottom && 'opacity-100'
+		)}
+	></div>
+</ScrollArea>
