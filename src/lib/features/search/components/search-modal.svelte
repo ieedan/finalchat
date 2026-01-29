4ac04775c143c2a5b5ae46d06a97136bcf1cdfac
Aidan Bleser <aidanbleser35@gmail.com>
2026-01-29 08:41:33 -0600

    better ui

--- src/lib/features/search/components/search-modal.svelte
+++ src/lib/features/search/components/search-modal.svelte
@@ -8,13 +8,15 @@
 	import { shortcut } from '$lib/actions/shortcut.svelte';
 	import { goto } from '$app/navigation';
 	import MatchedText from './matched-text.svelte';
+	import { RiChatNewLine as MessagePlusIcon } from 'remixicon-svelte';
+	import { Skeleton } from '$lib/components/ui/skeleton';
 
 	let open = $state(false);
 	let search = $state('');
 
-	const searchDebounced = new Debounced(() => search, 50);
+	const searchDebounced = new Debounced(() => search, 150);
 
-	const messages = useQuery(api.chats.search, {
+	const messagesQuery = useQuery(api.chats.search, {
 		get query() {
 			return searchDebounced.current;
 		}
@@ -28,30 +30,60 @@
 		<Command.Root shouldFilter={false}>
 			<Command.Input wrapperClass="h-12" class="h-12" placeholder="Search..." bind:value={search} />
 			<Command.List>
-				{#if !messages.isLoading}
-					<Command.Empty>No results found</Command.Empty>
+				{#if !messagesQuery.isLoading && !searchDebounced.pending}
+					<Command.Group>
+						{#if !messagesQuery.isLoading && messagesQuery.data?.length === 0}
+							<Command.Item
+								class="py-2 px-4 flex items-center justify-between"
+								onSelect={() => {
+									goto(`/chat?q=${search}`);
+									open = false;
+									search = '';
+								}}
+							>
+								<div class="flex items-center gap-2">
+									<MessagePlusIcon class="inline size-4" />
+									Create new chat
+									<span class="text-muted-foreground">
+										{search}
+									</span>
+								</div>
+								<ArrowRightIcon />
+							</Command.Item>
+						{/if}
+						{#each messagesQuery.data ?? [] as result (result._id)}
+							<Command.Item
+								value={result._id}
+								class="flex items-center justify-between py-2 px-4"
+								onSelect={() => {
+									goto(`/chat/${result._id}`);
+									open = false;
+									search = '';
+								}}
+							>
+								<div class="flex items-start flex-col gap-0.5 flex-1">
+									<MatchedText class="text-foreground" match={result.matchedTitle} />
+									{#if result.matchedMessage}
+										<MatchedText class="text-muted-foreground" match={result.matchedMessage} />
+									{/if}
+								</div>
+								<ArrowRightIcon />
+							</Command.Item>
+						{/each}
+					</Command.Group>
+				{:else}
+					<Command.Group>
+						{#each Array(3) as _, i (i)}
+							<Command.Item class="flex h-14.5 items-center justify-between py-2 px-4" disabled>
+								<div class="flex items-start flex-col gap-0.5 flex-1">
+									<Skeleton class="h-4 w-3/4" />
+									<Skeleton class="h-3 w-1/2 mt-1" />
+								</div>
+								<Skeleton class="h-4 w-4 rounded" />
+							</Command.Item>
+						{/each}
+					</Command.Group>
 				{/if}
-				<Command.Group>
-					{#each messages.data ?? [] as result (result._id)}
-						<Command.Item
-							value={result._id}
-							class="flex items-center justify-between py-2 px-4"
-							onSelect={() => {
-								goto(`/chat/${result._id}`);
-								open = false;
-								search = '';
-							}}
-						>
-							<div class="flex items-start flex-col gap-0.5 flex-1">
-								<MatchedText class="text-foreground" match={result.matchedTitle} />
-								{#if result.matchedMessage}
-									<MatchedText class="text-muted-foreground" match={result.matchedMessage} />
-								{/if}
-							</div>
-							<ArrowRightIcon />
-						</Command.Item>
-					{/each}
-				</Command.Group>
 			</Command.List>
 		</Command.Root>
 	</Modal.Content>
